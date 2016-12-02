cecb90443fbcdec4d781dd7b8a85733b13539983
jaycverg <jaycverg@gmail.com>
2016-12-02 08:29:05 -0500

    initial commit

---
+++ app/Http/Controllers/Api/TestController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class TestController extends Controller
+{
+    function getIndex() {
+        return 'Hello world';
+    }
+}
